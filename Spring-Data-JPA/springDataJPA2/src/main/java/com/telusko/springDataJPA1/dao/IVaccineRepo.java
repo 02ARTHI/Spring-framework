8a47b6e75e1e41a94b7d6f2ea12e3ce90f8fa4c4
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-12 21:22:42 +0530

    Delete Spring-Data-JPA/springDataJPA2 directory

--- Spring-Data-JPA/springDataJPA2/src/main/java/com/telusko/springDataJPA1/dao/IVaccineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.springDataJPA1.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository //adding this annotation is optional 
-public interface IVaccineRepo extends CrudRepository<Vaccine, Integer> {
-
-}
