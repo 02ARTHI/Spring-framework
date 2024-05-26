576314732e4cd8e022850b9959f6d5bb3a082668
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-27 00:13:20 +0530

    Delete springDataJPADateTime directory

--- springDataJPADateTime/src/main/java/com/telusko/springDataJPA1/service/ICustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.telusko.springDataJPA1.service;
-
-import com.telusko.springDataJPA1.dao.CustomerInfo;
-
-public interface ICustomerService {
-	public String registerCustomer(CustomerInfo cx);
-}
