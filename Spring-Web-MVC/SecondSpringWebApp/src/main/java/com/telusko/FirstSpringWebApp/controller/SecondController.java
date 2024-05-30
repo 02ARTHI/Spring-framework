9c81b57c9fe96c287a915061ec0a775164190ddf
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-30 23:20:49 +0530

    springWebMVC with HTTP methods

---
+++ Spring-Web-MVC/SecondSpringWebApp/src/main/java/com/telusko/FirstSpringWebApp/controller/SecondController.java
@@ -0,0 +1,30 @@
+package com.telusko.FirstSpringWebApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+//@RequestMapping("controller2")
+public class SecondController {
+ 
+	@RequestMapping(value="/welcome", method=RequestMethod.GET) // get method by default
+	public String displaySomeMessage(Model model)
+	{
+		
+		model.addAttribute("message","focus is the key");
+		return "course"; //logical view name
+	}
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET) // get method by default
+	public void displayfewSomeMessage(Model model)
+	{
+		
+		model.addAttribute("message","focus is the key");
+		 //logical view name
+	}
+}
