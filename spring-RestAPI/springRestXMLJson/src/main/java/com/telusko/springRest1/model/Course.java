c60b422e2d18026e075634d4bf5996bd2d76710d
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-21 23:26:58 +0530

    Add files via upload

---
+++ spring-RestAPI/springRestXMLJson/src/main/java/com/telusko/springRest1/model/Course.java
@@ -0,0 +1,45 @@
+package com.telusko.springRest1.model;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Course {
+
+	private int cid;
+	private String cname;
+	private Double price;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Course(int cid, String cname, Double price) {
+		super();
+		this.cid = cid;
+		this.cname = cname;
+		this.price = price;
+	}
+	public Course() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Course [cid=" + cid + ", cname=" + cname + ", price=" + price + "]";
+	}
+	
+	
+}
