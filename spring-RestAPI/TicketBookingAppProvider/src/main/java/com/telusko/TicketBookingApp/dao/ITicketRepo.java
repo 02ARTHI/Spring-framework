c4b80bbcd9a818d047856839233635a2ad0fea68
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-25 23:21:28 +0530

    Add files via upload

---
+++ spring-RestAPI/TicketBookingAppProvider/src/main/java/com/telusko/TicketBookingApp/dao/ITicketRepo.java
@@ -0,0 +1,11 @@
+package com.telusko.TicketBookingApp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telusko.TicketBookingApp.model.Passenger;
+
+@Repository
+public interface ITicketRepo extends JpaRepository<Passenger, Integer> {
+
+}
