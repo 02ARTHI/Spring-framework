ba3cefee1ab2dfafd43b5413e777c2a3794cec0e
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-26 23:08:33 +0530

    Delete spring-RestAPI/TicketBookingAppProvider directory

--- spring-RestAPI/TicketBookingAppProvider/src/main/java/com/telusko/TicketBookingApp/service/ITicketService.java
+++
@@ -1,8 +0,0 @@
-package com.telusko.TicketBookingApp.service;
-
-import com.telusko.TicketBookingApp.model.Passenger;
-
-public interface ITicketService {
-  public Passenger register(Passenger pass);
-  public Passenger fetch(int id);
-}
