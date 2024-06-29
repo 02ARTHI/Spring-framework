db7422b7b36f26a43aa84f3b8082998bc9e5f8d3
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-29 23:30:52 +0530

    springBoot banner - customize your own banner

---
+++ springBootBannerApplication/src/main/java/com/telusko/learning/springBootBannerApplication/SpringBootBannerApplication.java
@@ -0,0 +1,16 @@
+package com.telusko.learning.springBootBannerApplication;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication();
+		app.setBannerMode(Mode.CONSOLE);
+		app.run(SpringBootBannerApplication.class, args);
+	}
+
+}
