5be47e5040bed2d761179f92a6c9576e95a5e56e
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-08-01 23:12:22 +0530

    app 1

---
+++ MicroServices/TeluskoApp1/src/main/java/com/telusko/service/TeluskoService.java
@@ -0,0 +1,12 @@
+package com.telusko.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeluskoService {
+
+	public String courseInfo()
+	{
+		return "java spring microservice course launched";
+	}
+}
