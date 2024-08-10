53c90b1a23204e93b9c16dc5abbcfdaad98574ca
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-08-11 00:24:26 +0530

    Add files via upload

---
+++ MicroServices/Spring-Cloud-config-server/App1/src/main/java/com/telusko/RestAppController.java
@@ -0,0 +1,22 @@
+package com.telusko;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestAppController 
+{
+	@Value("${msg}")
+	private String msg;
+	
+	@GetMapping("/course")
+	public String getCourseUpdate()
+	{
+		return msg;
+	}
+	
+	
+	
+
+}
